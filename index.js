d2016d9174d07dfdc91b3011100210215c8cdb40
Raí Joia <megac154@gmail.com>
2024-03-01 16:13:00 -0300

    feat: add calcula desconto e teste

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const somaHorasExtras = ( salario, valorHorasExtras ) => {
-  return salario + valorHorasExtras;
-}
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(1000, 100)); // 1100
+const calculaDesconto = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', 2200, calculaDesconto(2500, 300));
