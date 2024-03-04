51755eee283d1dcfc71e8b4e331211f196d11f32
Raí Joia <megac154@gmail.com>
2024-03-04 11:02:14 -0300

    feat: add test in group with describe

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-undef */
 import { somaHorasExtras, calculaDesconto } from '../index';
 
-test('Deve retornar o salário com as horas extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Testes para a folha de pagamento', () => {
+  it('Deve retornar o salário com as horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
-});
+    expect(retornado).toBe(esperado);
+  });
 
-test('Deve retornar o salário com os descontos', () => {
-  const esperado = 1800;
-  const retornado = calculaDesconto(2000, 200);
+  it('Deve retornar o salário com os descontos', () => {
+    const esperado = 1800;
+    const retornado = calculaDesconto(2000, 200);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
