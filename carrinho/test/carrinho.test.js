2edb06acde55e85a4eef1826eec80b1ffe3876bb
Raí Joia <megac154@gmail.com>
2024-03-05 10:03:31 -0300

    feat: testanto frete e finalização da compra

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -34,4 +34,26 @@ describe('Teste do carrinho', () => {
       'Carrinho de compras vazio',
     );
   });
+
+  it('Deve adicionar o frete', () => {
+    const carrinho = new Carrinho();
+    carrinho.adicionaFrete(30);
+    expect(carrinho.frete).toBe(30);
+  });
+
+  it('Deve finalizar as compras', () => {
+    const carrinho = new Carrinho();
+    const item = new Item('Camisa', 50, 3);
+    const item2 = new Item('Bermuda', 25, 3);
+
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+    carrinho.adicionaFrete(30);
+
+    expect(carrinho.finalizaCompra()).toStrictEqual({
+      subtotal: 225,
+      frete: 30,
+      total: 255,
+    });
+  });
 });
